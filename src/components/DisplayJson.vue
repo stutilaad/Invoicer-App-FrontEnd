e58c2ae76711702ee475698a921db333c40e97b2
stutilaad <stutilaad@gmail.com>
2020-08-07 04:56:24 +0530

    Custom table changes

--- src/components/DisplayJson.vue
+++ src/components/DisplayJson.vue
@@ -3,14 +3,27 @@
     <button type="button" v-on:click="displayBillers">List of Billers</button>
     <b-table :data="billers">
       <template slot-scope="props">
-        <b-table-column field="id" label="ID" width="40" numeric>{{ props.row.id }}</b-table-column>
+        <b-table-column field="id" label="ID">{{ props.row.id }}</b-table-column>
+        <b-table-column
+          field="name"
+          label="Name"
+        >{{ props.row.name.firstName }} {{props.row.name.lastName}}</b-table-column>
+        <b-table-column field="email" label="Email">{{ props.row.email }}</b-table-column>
+        <b-table-column field="phone" label="Phone">{{ props.row.phone }}</b-table-column>
+        <b-table-column
+          field="address"
+          label="Address"
+        >{{ props.row.address.street }}{{props.row.address.houseNo}}{{props.row.address.city}}{{props.row.address.postalCode}}{{props.row.address.country}}</b-table-column>
+        <b-table-column field="iban" label="IBAN">{{ props.row.iban }}</b-table-column>
+        <b-table-column field="company" label="Company">{{ props.row.company }}</b-table-column>
+        <b-table-column field="website" label="Website">{{ props.row.website }}</b-table-column>
       </template>
     </b-table>
   </div>
 </template>
 <script>
 import axios from "axios";
-const baseUrl = process.env.API_SERVER;
+const baseUrl = process.env.VUE_APP_API_SERVER;
 export default {
   name: "DisplayJson",
   data() {
@@ -22,7 +35,7 @@ export default {
   methods: {
     displayBillers() {
       axios
-        .get(baseUrl + '/biller')
+        .get(baseUrl + "/biller")
         .then(response => {
           console.log(response);
           this.$data.billers = response.data;
