85259a158b014ce2b9417bf5bfd1efd3319d60d2
stutilaad <stutilaad@gmail.com>
2020-08-17 21:00:36 +0530

    sidebar menu items

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
   <div id="app">
     <nav-bar></nav-bar>
-    <div class="columns">
-      <div class="column is-2">
-        <side-bar></side-bar>
-      </div>
-      <div class="column">
-        <router-view></router-view>
-      </div>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
 import NavBar from "./components/NavBar";
-import SideBar from "./components/SideBar";
+
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 
@@ -24,8 +17,7 @@ Vue.use(Buefy);
 export default {
   name: "App",
   components: {
-    NavBar,
-    SideBar
+    NavBar
   }
 };
 </script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,14 +2,11 @@
   <b-navbar>
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <a href="#">
-          <img src="src/assets/logo.png" alt="logo" />
-          <!-- <strong>INVOICER-APP</strong> -->
-        </a>
+        <img src="src/assets/logo.png" alt="invoicer" />
       </b-navbar-item>
     </template>
     <template slot="start">
-      <b-navbar-item href="#">Home</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/dashboard' }">Home</b-navbar-item>
       <b-navbar-item tag="router-link" :to="{ path: '/billerlist' }">Biller-List</b-navbar-item>
     </template>
 
